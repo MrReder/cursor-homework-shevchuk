7b2c7b3b2fbfe7125d4de299cd4e8f198191a1d6
MrReder <73144687+MrReder@users.noreply.github.com>
2021-02-23 15:03:21 +0200

    Add files via upload

--- homework2/main.js
+++ homework2/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 const valueN = parseInt(prompt('Enter value N. It should be integer number'));
 Number.isInteger(valueN) === true ? alert(`Number N is ${valueN}.`) : alert(`Wrong input of number N. Try again!`);
 
@@ -25,7 +25,39 @@ if (isPair === true) {
     }
 }
 document.writeln(`Amount of numbers from ${valueN} to ${valueM} (included) is ${numbersSum}.`);
+*/
 
+let valueN = +prompt('Enter value N. It should be integer number');
+//Number.isInteger(valueN) === true ? alert(`Number N is ${valueN}.`) : alert(`Wrong input of number N. Try again!`);
+while (!Number.isInteger(valueN)) {
+    valueN = prompt('Number must be integer');
+}
+
+let valueM = +prompt('Enter value M. It should be integer number too');
+//Number.isInteger(valueM) === true ? alert(`Number M is ${valueM}.`) : alert(`Wrong input of number M. Try again!`);
+while (!Number.isInteger(valueM)) {
+    valueM = prompt('Number must be integer');
+}
+const wrongInput = valueN >= valueM ? prompt(`Number M must be more than number N. Press F5 and try again!`) : isPair;
+
+let isPair = confirm('Would we do miss even numbers?');
+
+
+
+let numbersSum = 0;
+
+if (isPair) {
+    for (let i = valueN; i <= valueM; i++) {
+        if (i % 2 != 0) {
+            numbersSum += i;
+        }
+    }
+} else {
+    for (let i = valueN; i <= valueM; i++) {
+        numbersSum += i;
+    }
+}
+document.writeln(`Amount of numbers from ${valueN} to ${valueM} (included) is ${numbersSum}.`);
 
 
 
