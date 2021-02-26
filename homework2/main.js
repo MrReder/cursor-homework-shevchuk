42926e9e616b550159562f900f6527997f9d4547
MrReder <73144687+MrReder@users.noreply.github.com>
2021-02-26 21:39:48 +0200

    Add files via upload

--- homework2/main.js
+++ homework2/main.js
@@ -1,46 +1,20 @@
 'use strict';
 
-/*
-const valueN = parseInt(prompt('Enter value N. It should be integer number'));
-Number.isInteger(valueN) === true ? alert(`Number N is ${valueN}.`) : alert(`Wrong input of number N. Try again!`);
-
-const valueM = parseInt(prompt('Enter value M. It should be integer number too'));
-Number.isInteger(valueM) === true ? alert(`Number M is ${valueM}.`) : alert(`Wrong input of number M. Try again!`);
-
-let isPair = confirm('Would we do miss even numbers?') ? true : false;
-
-const wrongInput = valueN >= valueM ? alert(`Number M must be more than number N. Press F5 and try again!`) : isPair;
-
-let numbersSum = 0;
-
-if (isPair === true) {
-    for (let i = valueN; i <= valueM; i++) {
-        if (i % 2 != 0) {
-            numbersSum += i;
-        }
-    }
-} else {
-    for (let i = valueN; i <= valueM; i++) {
-        numbersSum += i;
-    }
-}
-document.writeln(`Amount of numbers from ${valueN} to ${valueM} (included) is ${numbersSum}.`);
-*/
-
 let valueN = +prompt('Enter value N. It should be integer number');
-//Number.isInteger(valueN) === true ? alert(`Number N is ${valueN}.`) : alert(`Wrong input of number N. Try again!`);
-while (!Number.isInteger(valueN)) {
-    valueN = prompt('Number must be integer');
+while (!Number.isInteger(valueN) || valueN === 0) {
+    valueN = +prompt('Enter an integer start number, please');
 }
 
 let valueM = +prompt('Enter value M. It should be integer number too');
-//Number.isInteger(valueM) === true ? alert(`Number M is ${valueM}.`) : alert(`Wrong input of number M. Try again!`);
-while (!Number.isInteger(valueM)) {
-    valueM = prompt('Number must be integer');
+while (!Number.isInteger(valueM) || valueN >= valueM || valueM === 0) {
+    if (valueN >= valueM) {
+        valueM = +prompt('Enter a valueM greater that a N value');
+    } else {
+        valueM = +prompt('Enter an integer valueM');
+    }
 }
-let isPair = confirm('Would we do miss even numbers?');
 
-const wrongInput = valueN >= valueM ? prompt(`Number M must be more than number N. Press F5 and try again!`) : isPair;
+let isPair = confirm('Would we do miss even numbers?');
 
 let numbersSum = 0;
 
