c46c2eca44bead240f9db1676f90b6b2f06782e8
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-04 00:28:49 +0200

    Update main.js

--- homework4/main.js
+++ homework4/main.js
@@ -6,31 +6,21 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 //1
 
-const getPairs = function () {
-    let pairArr =
-        [[students.slice(0, 2).join(" та ")],
+const getPairs = () => {
+    const arr = [
+        [students.slice(0, 2).join(" та ")],
         [students.slice(2, 4).join(" та ")],
-        [students.slice(4, 6).join(" та ")]];
-    return pairArr = pairArr.join(", ");
+        [students.slice(4, 6).join(" та ")]
+    ];
+    return arr;
 }
-console.log(`Пари студентів ${getPairs()}`);
+console.log(`Пари студентів`, getPairs());
 
 //2 Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. 
 //Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
 
-const getPairsProjects = function () {
-    /*
-        let pairArr =
-            [[students.slice(0, 2).join(" та ")],
-            [students.slice(2, 4).join(" та ")],
-            [students.slice(4, 6).join(" та ")]];
-    
-        let pairProjects =
-            [[pairArr[0].concat(themes[0])],
-            [pairArr[1].concat(themes[1])],
-            [pairArr[2].concat(themes[2])]];
-        return pairProjects = pairProjects.join(" , ");
-        */
+const getPairsProjects = () => {
+
 
     let pairArr =
         [[students.slice(0, 2).join(" та ")],
@@ -40,43 +30,44 @@ const getPairsProjects = function () {
     let i = 0;
     let pairsProjects = [];
     for (let i = 0; i < themes.length; i++) {
-        pairsProjects.push(pairArr[i], themes[i]);
+        pairsProjects.push(pairArr[i].concat(themes[i]));
     } return pairsProjects;
 
 }
 
-console.log(`Теми проектів для пар: ${getPairsProjects()}`); //есть проблемы со знаками препинания :(( В закоментеном варианте такого нет...
+console.log(`Теми проектів для пар`, getPairsProjects());
 
 //3 Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
 
-const compareStudentsMarks = function () {
-    let studentsMarks = [[students[0].concat(marks[0])],
-    [students[1].concat(marks[1])],
-    [students[2].concat(marks[2])],
-    [students[3].concat(marks[3])],
-    [students[4].concat(marks[4])],
-    [students[5].concat(marks[5])]]
-    return studentsMarks = studentsMarks.join(", ");
+const compareStudentsMarks = () => {
 
+    const studentsMarks = [];
+
+    let i = 0;
+
+    for (let i = 0; i < students.length; i++) {
+        studentsMarks.push([students[i], marks[i]]);
+    } return studentsMarks;
 }
-console.log(compareStudentsMarks());
+console.log('Студенти та оцінки', compareStudentsMarks());
 
 //4 Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт
 //(тут функція буде нечистою, але не повинна мутувати массив): [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]]
 
 const getRandomMark = function () {
     let randomMarksArr = [];
-    const getRandomMarks = function (max) {
-        return Math.ceil(Math.random() * max);
-    }
+
+    let getRandomMarks = (min, max) => {
+        let randomMark = Math.ceil(min + Math.random() * (max + 1 - min));
+        return randomMark;
+    };
 
     for (let i = 0; i < getPairsProjects().length; i++) {
-        let pairsProjectsMarks = [getPairsProjects(i), getRandomMarks(5)];
-
-        return randomMarksArr.push(pairsProjectsMarks);
-
+        let pairsProjectsMarks = [getPairsProjects(i).map(getPairsProjects(i).concat(getRandomMarks(1, 5)))];
+        return pairsProjectsMarks;
     }
-
+    return randomMarksArr.push(pairsProjectsMarks);
 }
 
 console.log(getRandomMark());
+
