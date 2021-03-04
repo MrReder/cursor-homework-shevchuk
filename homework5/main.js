7614dda72dd62141d416f7d3e07e1f0c813c0fa4
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-04 14:18:21 +0200

    Update main.js

--- homework5/main.js
+++ homework5/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+//1
+
+const getRandomArray = (length, min, max) => {
+
+    let randomArr = [];
+
+    for (let i = 0; i < length; i++) {
+
+        let getRandomNumber = (minNum, maxNum) => {
+            let randomNum = Math.ceil(Math.random() * (maxNum + 1 - minNum));
+            return randomNum;
+        };
+
+        randomArr.push(getRandomNumber(min, max));
+
+    } return randomArr;
+}
+
+console.log(getRandomArray(22, 5, 97));
