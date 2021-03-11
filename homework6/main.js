903f00e3e24a24ccba6dfa773dabe1e7b78681bc
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-11 17:28:27 +0200

    Update main.js

--- homework6/main.js
+++ homework6/main.js
@@ -128,3 +128,23 @@ const getBestStudent = (students) => {
 console.log(`#5 And the best student is: ${getBestStudent(students)}! 
 ${getBestStudent(students)}, please accept our congratulations!!!`);
 
+//6 Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.
+const calculateWordLetters = (word) => {
+    let countLetter = function (letter, word) {
+        let counter = 0;
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] === letter) {
+                counter++;
+            }
+        } return counter;
+    }
+
+    let objectWithKeys = {};
+    let wordArr = [];
+    wordArr = word.split('');
+    for (let i = 0; i < wordArr.length; i++) {
+        let obj = {};
+        obj = Object.assign(wordArr[i]);
+    }
+}
+console.log(calculateWordLetters('The Subways'));
