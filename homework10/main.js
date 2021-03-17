a407a140a9adaa4e64e36e8223e6328c26bca24d
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-17 22:25:42 +0200

    Update main.js

--- homework10/main.js
+++ homework10/main.js
@@ -42,24 +42,16 @@ function generateBlocks() {
 }
 generateBlocks();
 
-
+//I can`t make it
 function generateBlocksInterval() {
-    let intervalBlocks = document.createElement('section');
-    intervalBlocks.classList.add('interval-blocks');
-    intervalBlocks = document.querySelector('.cubics-wrapper');
-    intervalBlocks.style.cssText = `display: grid;
-justify-content: center;
-grid-template-columns: 50px 50px 50px 50px 50px;
-grid-template-rows: 50px 50px 50px 50px;
-margin-top: 50px}`;
     for (let i = 0; i < 25; i++) {
         let block = document.createElement('div');
         block.className = 'single-block';
         block.style.cssText = `height: 50px;
         width: 50px;
         border: solid 2px`;
-        block.style.backgroundColor = setInterval("backgroundColor()", 1000);
-        intervalBlocks.appendChild(block);
+        block.style.backgroundColor = backgroundColor();
+        blocks.appendChild(block);
     }
 }
 
