cd65746b93aa5dcb3687e2f7cea12f30cfd77da8
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-19 22:41:36 +0200

    Update main.js

--- homework10/main.js
+++ homework10/main.js
@@ -9,20 +9,6 @@ grid-template-rows: 50px 50px 50px 50px;
 margin-top: 50px;
 margin-bottom: 50px}`;
 
-
-/*
-let block = document.querySelector('.single-cubic');
-block.style.cssText = `height: 50px;
-width: 50px;
-border: solid 2px;`;
-block.style.backgroundColor = function backgroundColor() {
-    let red = Math.ceil(0 + Math.random() * (255 + 1 - 0));
-    let green = Math.ceil(0 + Math.random() * (255 + 1 - 0));
-    let blue = Math.ceil(0 + Math.random() * (255 + 1 - 0));
-    return `rgb(${red},${green},${blue})`;
-};
-*/
-
 function backgroundColor() {
     let red = Math.ceil(0 + Math.random() * (255 + 1 - 0));
     let green = Math.ceil(0 + Math.random() * (255 + 1 - 0));
