2d84ef2b6ba40f968a0e984591a5afa695731046
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-22 23:43:23 +0200

    hw11

--- homework11/main.js
+++ homework11/main.js
@@ -1,2 +1,8 @@
 "use strict";
 
+const keys = document.querySelector('.keys-wrapper');
+
+keys.style.cssText = `display: grid;
+justify-content: center;
+grid-template-columns: 50px 50px 50px 50px 50px; 50px;
+grid-template-rows: 50px 50px 50px 50px 50px 50px;`;
