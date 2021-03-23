424ab8b8e93c7acca4f52be715e1280282bba406
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-23 08:38:50 +0200

    inProgress

--- homework11/main.js
+++ homework11/main.js
@@ -1,8 +1,33 @@
 "use strict";
 
-const keys = document.querySelector('.keys-wrapper');
+/*
+const buttons = document.querySelector('.keys-wrapper');
 
-keys.style.cssText = `display: grid;
+buttons.style.cssText = `display: grid;
 justify-content: center;
-grid-template-columns: 50px 50px 50px 50px 50px; 50px;
-grid-template-rows: 50px 50px 50px 50px 50px 50px;`;
+grid-template-columns: 100px 100px 100px 100px 100px 10px;
+grid-template-rows: 100px 100px 100px 100px 100px 100px;`;
+
+function generateBlocksKeys() {
+    for (let i = 0; i < 12; i++) {
+        const button = document.createElement('div');
+        button.className = 'singleBtn';
+        button.style.cssText = `height: 100px;
+        width: 100px;
+        border: solid 2px;
+        background-color: red;
+        margin: 20px 20px;`;
+        buttons.appendChild(button);
+        const key = document.createElement('kbd');
+        button.appendChild(key)
+        function displayKbds(...values) {
+            return key.textContent = values;
+        }
+        displayKbds('q', 'w', 'e', 'r', 't', 'y', 'a', 's', 'd', 'f', 'g', 'h');
+    }
+
+}
+generateBlocksKeys();
+*/
+
+
