feeca39a3ca10b3d831a78e3f1c3d48e4440c14d
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-24 06:24:38 +0200

    hw11

--- homework11/main.js
+++ homework11/main.js
@@ -19,34 +19,30 @@
 // });
 
 //It`s not work
-// function getSound() {
-//     const btns = document.querySelectorAll('.single-button');
-//     const btnsArr = [];
-//     btnsArr.push(btns);
-//     const sounds = document.querySelectorAll('audio');
-//     const soundsArr = [];
-//     soundsArr.push(sounds);
-//     for (let i = 0; i < btnsArr.length; i++) {
-//         for (let j = 0; j < soundsArr.length; j++) {
-//             document.body.addEventListener('mousedown', function (e) {
-//                 e.btnsArr[i].style.backgroundColor = 'crimson';
-//                 e.soundsArr[j].play();
-//             });
-//             document.body.addEventListener('mouseup', function (e) {
-//                 e.btnsArr[i].style.backgroundColor = 'transparent';
-//             });
+function getSound() {
+    const btns = document.querySelectorAll('.single-button');
+    const btnsArr = [];
+    btnsArr.push(btns);
+    const sounds = document.querySelectorAll('audio');
+    const soundsArr = [];
+    soundsArr.push(sounds);
+    for (let i = 0; i < btnsArr.length; i++) {
+        for (let j = 0; j < soundsArr.length; j++) {
+            document.body.addEventListener('keydown', function () {
+                btnsArr[i].style.backgroundColor = 'crimson';
+                soundsArr[j].play();
+            });
+            document.body.addEventListener('keyup', function () {
+                btnsArr[i].style.backgroundColor = 'transparent';
+            });
 
-//         }
+        }
 
-//     }
-// }
-// getSound();
+    }
+}
+getSound();
 
-document.body.addEventListener('keydown', function (e) {
-    let soundOgg = new Audio(`./sound/sound${e.key.toUpperCase()}.ogg`);
-    soundOgg.play();
-})
-document.body.addEventListener('keydown', function (e) {
-    let sound = new Audio(`./sound/sound${e.key.toUpperCase()}.mp3`);
-    sound.play();
-})
+// document.body.addEventListener('keydown', function (e) {
+//     let soundOgg = new Audio(`./sound/sound${e.key.toUpperCase()}.ogg`);
+//     soundOgg.play();
+// })
