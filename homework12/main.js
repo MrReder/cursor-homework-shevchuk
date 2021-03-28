322731dc56c87927a72c1f23c17d50ba4383da16
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-28 20:13:18 +0300

    Update main.js

--- homework12/main.js
+++ homework12/main.js
@@ -3,7 +3,7 @@
 function getRandomChinese(length) {
     return new Promise((resolve) => {
         for (let i = 0; i < length; i++) {
-            const sign = +(Date.now().toString().substr(8, 12));
+            const sign = +(Date.now(i).toString().substr(8, 12));
             let glyphs = ' ';
             glyphs += String.fromCharCode(sign);
             setTimeout(() => {
