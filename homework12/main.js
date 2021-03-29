17d0b7535496f9484e0fe35e2dc49f4dff338756
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-29 21:41:27 +0300

    Update main.js

--- homework12/main.js
+++ homework12/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//It works in both ways. But in both ways It is displaying not very correct as for me. What is my mistake?
+
 function getRandomChinese(length) {
     return new Promise((resolve) => {
         for (let i = 0; i < length; i++) {
@@ -16,20 +18,22 @@ function getRandomChinese(length) {
 
 getRandomChinese(4);
 
-// function getRandomChinese(length) {
-//     return new Promise((resolve) => {
+// async function getRandomChinese(length) {
+//     await new Promise((resolve) => {
 //         let i = 0;
 //         while (i < length) {
-//             const sign = +(Date.now(i).toString().substr(8, 12));
-//             let glyphs = ' ';
-//             glyphs += String.fromCharCode(sign);
-//             setTimeout(() => {
-//                 console.log(glyphs);
-//             }, length * 50)
+//             const sign = +(Date.now().toString().substr(8, 12));
+//             let glyphs = [];
+//             glyphs.push(String.fromCharCode(sign));
 //             i++;
-//             resolve();
+//             setTimeout(() => {
+//                 resolve(glyphs);
+//             }, length * 50)
+//             console.log(glyphs.toString());
 //         }
+
 //     })
+
 // }
 
 // getRandomChinese(4);
