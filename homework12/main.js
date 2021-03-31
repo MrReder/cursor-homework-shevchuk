7cab2823b29b55f9af4f39c3c7855cda80b5e94a
MrReder <73144687+MrReder@users.noreply.github.com>
2021-03-31 19:03:27 +0300

    Update main.js

--- homework12/main.js
+++ homework12/main.js
@@ -1,39 +1,29 @@
 "use strict";
 
-//It works in both ways. But in both ways It is displaying not very correct as for me. What is my mistake?
-
 function getRandomChinese(length) {
     return new Promise((resolve) => {
-        for (let i = 0; i < length; i++) {
-            const sign = +(Date.now(i).toString().substr(8, 12));
-            let glyphs = ' ';
-            glyphs += String.fromCharCode(sign);
-            setTimeout(() => {
-                console.log(glyphs);
-            }, length * 50)
-            resolve();
-        }
+        const sign = String.fromCharCode(Date.now().toString().substr(8, 12));
+        setTimeout(() => {
+            resolve(sign);
+        }, length * 50)
     })
 }
 
-getRandomChinese(4);
+async function getWholeWord(length) {
+    let word = '';
+    let i = 0;
+    while (i < length) {
+        await getRandomChinese(length).then((resolve) => {
+            word += resolve;
+        })
+        i++
+    }
+    console.log(word)
+}
 
-// async function getRandomChinese(length) {
-//     await new Promise((resolve) => {
-//         let i = 0;
-//         while (i < length) {
-//             const sign = +(Date.now().toString().substr(8, 12));
-//             let glyphs = [];
-//             glyphs.push(String.fromCharCode(sign));
-//             i++;
-//             setTimeout(() => {
-//                 resolve(glyphs);
-//             }, length * 50)
-//             console.log(glyphs.toString());
-//         }
+getWholeWord(4);
 
-//     })
 
-// }
 
-// getRandomChinese(4);
+
+
