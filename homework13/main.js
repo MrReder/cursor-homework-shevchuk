9a87e300488e28bea5d4779c5e58dcd37a236138
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-01 11:14:08 +0300

    Update main.js

--- homework13/main.js
+++ homework13/main.js
@@ -10,36 +10,29 @@ async function getNFO() {
             return res.data
         });
     const charactersUrls = episode5Data.characters;
-    const charactersArr = [];
-    charactersArr.push(charactersUrls);
-    console.log(charactersArr);
     // const firstPerson = await axios.get('http://swapi.dev/api/people/1/')
     //     .then((res) => {
     //         return res.data.name
     //     })
     // console.log(firstPerson);
-    for (let i = 0; i < charactersArr; i++) {
-        const names = await axios.get(`${charactersArr[i]}`)
+    for (let i = 0; i < charactersUrls; i++) {
+        const names = await axios.get(`https${charactersUrls[i].substring(4)}`)
             .then((res) => {
-                return res.data.names
+                res.charactersUrls.map((e) => {
+                    return console.log(e.data.name);
+                })
             })
+
         console.log(names);
     }
-    //     const infoObj = {
-    //         name: await axios.get(`https${charactersArr[i]}`)
-    //             .then((res) => {
-    //                 return res.data.name
-    //             }),
-    //         birthYear: await axios.get(`https${charactersArr[i]}`)
-    //             .then((res) => {
-    //                 return res.data.birth_year
-    //             }),
-    //         gender: await axios.get(`https${charactersArr[i]}`)
-    //             .then((res) => {
-    //                 return res.data.gender
-    //             }),
-    //     }
+
+    // for (let i = 0; i < charactersUrls; i++) {
+    //     const names = await axios.get(`https${charactersUrls[i].substring(4)}`)
+    //         .then((res) => {
+    //             console.log(res.data.names)
+    //         })
     // }
+
 
 }
 
