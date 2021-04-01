8f5b12b81d2f45df8095ef9ca701ed73a7126461
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-01 17:30:20 +0300

    Update main.js

--- homework13/main.js
+++ homework13/main.js
@@ -10,7 +10,6 @@ async function getNFO() {
             return res.data
         });
     const charactersUrls = episode5Data.characters;
-
     let personsObj = {};
 
     for (let i = 0; i < charactersUrls.length; i++) {
@@ -21,33 +20,12 @@ async function getNFO() {
         personsObj[i] = {
             name: peopleData.name,
             birthYear: peopleData.birth_year,
-            gender: peopleData.gender
+            gender: peopleData.gender,
         }
-        console.log(personsObj[i]);
+        const personsArr = [];
+        personsArr.push(personsObj)
+        console.log(personsArr)
 
-        // const persons = docuemnt.querySelector('.people-wrapper');
-
-        // persons.style.cssText = `display: grid;
-        // justify-content: center;
-        // grid-template-columns: 50px 50px 50px 50px;
-        // grid-template-rows: 50px 50px 50px 50px;
-        // margin-top: 50px;
-        // margin-bottom: 50px}`;
-        // function generatePersonsList() {
-        //     for (let i = 0; i < 16; i++) {
-        //         const person = document.createElement('div');
-        //         person.style.cssText = `height: 50px;
-        //         width: 50px;
-        //         border: solid 2px`;
-        //         persons.appendChild(person);
-        //     }
-        // }
-        // generatePersonsList();
-        // firstBtn.addEventListener('click', function () {
-        //     generateBlocks();
-        //     const container = document.querySelector('.buttons-container');
-        //     container.removeChild(firstBtn);
-        // });
     }
 
 
