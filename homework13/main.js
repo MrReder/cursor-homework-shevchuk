6e4a08000225078a71ed43798d66c0c52e11814e
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-02 22:36:22 +0300

    upd

--- homework13/main.js
+++ homework13/main.js
@@ -31,7 +31,9 @@ async function getNFO() {
         grid-template-rows: 220px 220px 220px 220px;
         padding-left: 120px;
         position: relative;
+        -webkit-user-modify: read-write-plaintext-only;
         z-index: 3`;
+
         function generatePersonsDivs() {
             const person = document.createElement('div');
             person.style.cssText = `height: 200px;
@@ -53,5 +55,60 @@ async function getNFO() {
     }
 }
 
+
+// const getNFOBtn = document.querySelector('.get-info-btn');
+// getNFOBtn.addEventListener('click', getNFO);
+
+// async function getNFO() {
+//     const backgroundSound = document.querySelector('audio');
+//     backgroundSound.play();
+//     const episode5Data = await axios.get('https://swapi.dev/api/films/2/')
+//         .then((res) => {
+//             return res.data
+//         });
+//     const charactersUrls = episode5Data.characters;
+//     let personsObj = {};
+
+//     for (let i = 0; i < charactersUrls.length; i++) {
+//         const peopleData = await axios.get(charactersUrls[i])
+//             .then((res) => {
+//                 return res.data
+//             })
+//         personsObj = {
+//             name: peopleData.name,
+//             birthYear: peopleData.birth_year,
+//             gender: peopleData.gender
+//         }
+
+//         const persons = document.querySelector('.people-wrapper');
+//         persons.style.cssText = `display: grid;
+//         justify-content: center;
+//         grid-template-columns: 450px 450px 450px 450px;
+//         grid-template-rows: 220px 220px 220px 220px;
+//         padding-left: 120px;
+//         position: relative;
+//         -webkit-user-modify: read-write-plaintext-only;
+//         z-index: 3`;
+
+//         function generatePersonsDivs() {
+//             const person = document.createElement('div');
+//             person.style.cssText = `height: 200px;
+//             width: 300px;
+//             border: solid 2px;
+//             border-color: rgb(172, 104, 3);
+//             text-align: right;
+//             font-size: 24px;
+//             font-weight: bolder;
+//             margin-right: 15px;
+//             align-items: space-around;
+//             background: linear-gradient(to bottom right, rgb(172, 104, 3), rgb(252, 252, 208))`;
+//             persons.appendChild(person);
+//             person.innerHTML = `${personsObj.name} <br>
+//             ${personsObj.birthYear} <br>
+//             ${personsObj.gender}`;
+//         }
+//         generatePersonsDivs();
+//     }
+// }
 
 
