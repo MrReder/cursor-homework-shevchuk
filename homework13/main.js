18d1c57e74bc1df882317c69b1b5a7b156a5ab86
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-04 20:27:02 +0300

    Update main.js

--- homework13/main.js
+++ homework13/main.js
@@ -104,6 +104,7 @@ async function getPlanets() {
         const planet = document.createElement('p');
         planet.classList.add('single-planet');
         planets.appendChild(planet);
+        planet.innerText = episode5DataPlanets;
 
     }
 
