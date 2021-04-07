e9101e9f655afcecad35e655d8af5c40314984d6
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-07 05:28:14 +0300

    Update main.js

--- homework15/main.js
+++ homework15/main.js
@@ -1,2 +1,13 @@
 "use strict";
 
+function* createIdGenerator() {
+    while (true) {
+        yield 1
+    }
+}
+
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
