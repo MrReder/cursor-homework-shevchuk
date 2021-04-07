8c7de1a11993ad5cb2b6f64c2c5f973ca70bca4c
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-07 05:36:19 +0300

    Update main.js

--- homework15/main.js
+++ homework15/main.js
@@ -1,13 +1,16 @@
 "use strict";
 
-function* createIdGenerator() {
-    while (true) {
-        yield 1
+function* createIdGenerator(start = 0, end = Infinity, step = 1) {
+    let iterationCount = 0;
+    for (let i = start; i < end; i += step) {
+        iterationCount++;
+        yield i;
     }
+    return iterationCount;
 }
+const idGenerator = createIdGenerator(1, Infinity, 1);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
 
-const idGenerator = createIdGenerator();
-
-console.log(idGenerator.next().value);
-console.log(idGenerator.next().value);
-console.log(idGenerator.next().value);
