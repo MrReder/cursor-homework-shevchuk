ca448c19e164d663e803f1e18f922ad790a67ca5
MrReder <73144687+MrReder@users.noreply.github.com>
2021-04-07 06:26:35 +0300

    upd

--- homework15/main.js
+++ homework15/main.js
@@ -14,3 +14,38 @@ console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
 
+
+
+// window.addEventListener('keydown', function* (e) {
+//     const btnUp = document.querySelector(`#up-btn[data-key="${e.keyCode}"]`);
+//     const btnDown = document.querySelector(`#down-btn[data-key="${e.keyCode}"]`);
+//     const increaseFontSize = function* (start = 14, end = 18, step = 2) {
+//         let fontSize = start;
+//         if (btnUp) {
+//             fontSize += step;
+//             yield document.body.h2.style.fontSize = `${fontSize}px`;
+//         } else if (btnDown) {
+//             fontSize -= step;
+//             yield document.body.h2.style.fontSize = `${fontSize}px`;
+//         }
+//         increaseFontSize()
+
+//     }
+// })
+
+window.addEventListener('click', function* (e) {
+    const btnUp = document.querySelector('#up-btn');
+    const btnDown = document.querySelector('#down-btn');
+    const increaseFontSize = function* (start = 14, end = 18, step = 2) {
+        let fontSize = start;
+        if (btnUp) {
+            fontSize += step;
+            yield h1.style.fontSize = `${fontSize}px`;
+        } else if (btnDown) {
+            fontSize -= step;
+            yield h1.style.fontSize = `${fontSize}px`;
+        }
+        increaseFontSize(14, 18, 2);
+
+    }
+})
